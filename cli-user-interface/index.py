4cc9a181c21778f1b928ad7ceb6022602e49fcac
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-08 17:59:03 -0500

    basic auth function implemented, no http request is being made yet

--- cli-user-interface/index.py
+++ cli-user-interface/index.py
@@ -1,10 +1,23 @@
+logged_in = False
+
+def auth():
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        global logged_in
+        if username == 'test':
+            logged_in = True
+
 def app():
     while True:
         cmd = input('type your instruction then hit Enter\n')
         # Exit condition
         if cmd == 'q':
             break
+        else:
+            print("You entered: " + cmd)
 
 if __name__ == "__main__":
     print("Hey ya!")
+    while not logged_in:
+        auth()
     app()
