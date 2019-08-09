f86e34f8b92f50b7b54ce2419d41d25eaa7cad71
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-08 21:35:48 -0500

    minor tweaks

--- cli-user-interface/index.py
+++ cli-user-interface/index.py
@@ -2,15 +2,12 @@ import requests
 import json
 import os
 
-from env import AUTH_URL
+from env import LAMBDAS
 
 logged_in = False
 
 def auth():
-    try:
-        url = os.environ['AUTH_URL'] = AUTH_URL
-    except KeyError:
-        print("AUTH_URL environment variable is not set")
+    global url
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     
@@ -21,11 +18,11 @@ def auth():
         "password": password
     }
 
-    response = requests.post(url, json=body, headers=headers).json()
+    response = requests.post(f"{url}/auth", json=body, headers=headers).json()
 
     print(response)
     global logged_in
-    if response["username"]:
+    if "username" in response:
         logged_in = True
     else:
         print("Invalid credentials")
@@ -36,11 +33,34 @@ def app():
         # Exit condition
         if cmd == 'q':
             break
-        else:
+        elif len(cmd.split(' ')) == 3:
             print("You entered: " + cmd)
+            add(cmd)
+        else:
+            print("not a valid expression, must be in form (number operation number)")
+
+def add(cmd):
+    global url
+    
+    inputs = cmd.split(" ")
+    print(inputs[0])
+    body = {
+        "x": int(inputs[0]),
+        "y": int(inputs[2])
+    }
+
+    headers = {"content-type": "application/json"}
+
+    response = requests.post(f"{url}/add", json=body, headers=headers).json()
+
+    print(response)
 
 if __name__ == "__main__":
     print("Hey ya!")
+    try:
+        url = os.environ["LAMBDAS"] = LAMBDAS
+    except KeyError:
+        print("AUTH_URL environment variable is not set")
     while not logged_in:
         auth()
     app()
